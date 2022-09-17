234b85fd3360fadd2ee69ed3c483856840a842f5
patrickrasabonihantana2 <patrickrasabonihantana2@gmail.com>
2022-09-17 20:14:36 +0300

    [fix] route login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'login',
+    path: 'auth',
     loadChildren: () => import('./page/auth/auth.module').then(m => m.AuthModule)
   },
   {
