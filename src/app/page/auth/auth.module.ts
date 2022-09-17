05e1788ed8ae68bde64a66b6c4c6cda2588f0474
patrickrasabonihantana2 <patrickrasabonihantana2@gmail.com>
2022-09-17 19:46:58 +0300

    auth module + tailwind css

---
+++ src/app/page/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { AuthService } from './auth.service';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './login/login.component';
+
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthRoutingModule
+  ],
+  providers: [
+    AuthService
+  ]
+})
+export class AuthModule { }
